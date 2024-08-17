0f42b1a50aa0b5ef5a44c799f2ac8a25eb57b4df
WuJiaYi <30690400+HelloWuJiaYi@users.noreply.github.com>
2024-08-17 14:55:48 +0800

    Update Plugin.php
    1. 增加使用说明
    2. 增加默认显示目录选项

--- Plugin.php
+++ Plugin.php
@@ -1,5 +1,7 @@
 <?php
 if (!defined('__TYPECHO_ROOT_DIR__')) exit;
+
+
 /**
  * 文章目录：
  * 
@@ -9,7 +11,7 @@ if (!defined('__TYPECHO_ROOT_DIR__')) exit;
  * 
  * @package PostToc
  * @author 吴佳轶
- * @version 1.0.0
+ * @version 1.1.0
  * @link https://www.wujiayi.vip
  */
 class PostToc_Plugin implements Typecho_Plugin_Interface
@@ -22,13 +24,37 @@ class PostToc_Plugin implements Typecho_Plugin_Interface
 
     public static function deactivate(){}
 
-    public static function config(Typecho_Widget_Helper_Form $form){}
+    public static function config(Typecho_Widget_Helper_Form $form)
+    {
+        // 文本说明
+        $instructions = new Typecho_Widget_Helper_Form_Element_Textarea(
+            'instructions', 
+            NULL, 
+            '<!-- 隐藏/显示目录按钮 -->
+<button id="toc-toggle" class="toc-toggle"></button>',
+            _t('设置按钮'),  // 这是文本框的标题
+            _t('在 post.php 文件中的是<a href="https://github.com/HelloWuJiaYi/PostToc" target="_blank" style="color: #ff5500;">适当位置</a>插入以上代码，用来设置 “隐藏/显示目录"的按钮。')
+        );
+        $instructions->input->setAttribute('style', 'width: 100%; height: 100%; background-color: #f9f9f9; border: 1px solid #ddd;');
+        $instructions->input->setAttribute('readonly', 'readonly');
+        $form->addInput($instructions);
+        
+        // 表单选项
+        $defaultDisplay = new Typecho_Widget_Helper_Form_Element_Radio(
+            'defaultDisplay', 
+            array('1' => '是', '0' => '否'),
+            '1', 
+            _t('默认显示目录'),  // 这是单选按钮的标题
+            _t('选择文章页面加载时是否显示目录。')
+        );
+        $form->addInput($defaultDisplay);
+    }
+
 
     public static function personalConfig(Typecho_Widget_Helper_Form $form){}
 
     public static function renderHeader()
     {
-        // 仅在普通文章页面加载样式
         $widget = Typecho_Widget::widget('Widget_Archive');
         if ($widget->is('single') && !$widget->is('page')) {
             echo '<link rel="stylesheet" href="' . Helper::options()->pluginUrl . '/PostToc/static/style.css">';
@@ -37,9 +63,12 @@ class PostToc_Plugin implements Typecho_Plugin_Interface
 
     public static function renderFooter()
     {
-        // 仅在普通文章页面加载脚本
         $widget = Typecho_Widget::widget('Widget_Archive');
         if ($widget->is('single') && !$widget->is('page')) {
+            $defaultDisplay = Typecho_Widget::widget('Widget_Options')->plugin('PostToc')->defaultDisplay;
+            echo '<script>';
+            echo 'var defaultDisplay = ' . json_encode($defaultDisplay) . ';';
+            echo '</script>';
             echo '<script src="' . Helper::options()->pluginUrl . '/PostToc/static/script.js"></script>';
         }
     }
